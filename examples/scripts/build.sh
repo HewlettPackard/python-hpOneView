3e189b0a7ffdd574515c5c5cf302dcde228eef10
Troy Heber <troy@debdev.org>
2015-03-09 10:08:01 -0600

    Fix typo in build.sh

--- examples/scripts/build.sh
+++ examples/scripts/build.sh
@@ -45,7 +45,7 @@ echo  -- Import Enclosures
 echo  -- Add Storage System
 ./add-storage-system.py -a $OV_HOST -u $OV_USER -p $OV_PASS -s $STO_ADDR -su $STO_USR -sp $STO_PASS
 echo  -- Defining Storage Pools
-./add-storage-pool.py -a $OV_HOST -u $OV_USER -p $OV_PAS -s ThreePAR7200-4446S -n SND_CPG1
+./add-storage-pool.py -a $OV_HOST -u $OV_USER -p $OV_PASS -s ThreePAR7200-4446S -n SND_CPG1
 echo  -- Defining Volume Templates
 ./add-volume-template.py -a $OV_HOST -u $OV_USER -p $OV_PASS -n "ESX Boot" -ss ThreePAR7200-4446 -sp SND_CPG1 -cap 50
 echo  -- Defining volumes
