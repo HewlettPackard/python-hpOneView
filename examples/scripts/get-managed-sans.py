5c77394f06d5f830dd637761e91c1f7282bad29d
Troy Heber <troy@debdev.org>
2015-04-03 10:58:39 -0600

    Add a description to the get-managed-sans example script help output.

--- examples/scripts/get-managed-sans.py
+++ examples/scripts/get-managed-sans.py
@@ -49,24 +49,34 @@ def login(con, credential):
         print('Login failed')
 
 
-def getsans(fcs):
+def get_managed_sans(fcs):
     sans = fcs.get_managed_sans()
     pprint(sans)
 
 
 def main():
-    parser = argparse.ArgumentParser(add_help=True, description='Usage')
-    parser.add_argument('-a', '--appliance', dest='host', required=True,
-                        help='HP OneView Appliance hostname or IP')
-    parser.add_argument('-u', '--user', dest='user', required=False,
-                        default='Administrator', help='HP OneView Username')
-    parser.add_argument('-p', '--pass', dest='passwd', required=True,
-                        help='HP OneView Password')
-    parser.add_argument('-c', '--certificate', dest='cert', required=False,
-                        help='Trusted SSL Certificate Bundle in PEM '
-                        '(Base64 Encoded DER) Format')
+    parser = argparse.ArgumentParser(add_help=True,
+                        formatter_class=argparse.RawTextHelpFormatter,
+                                     description='''
+    Display or list the available Managed SAN resources in the appliance
+
+    Usage: ''')
+    parser.add_argument('-a', dest='host', required=True,
+                        help='''
+    HP OneView Appliance hostname or IP address''')
+    parser.add_argument('-u', dest='user', required=False,
+                        default='Administrator',
+                        help='''
+    HP OneView Username''')
+    parser.add_argument('-p', dest='passwd', required=True,
+                        help='''
+    HP OneView Password''')
+    parser.add_argument('-c', dest='cert', required=False,
+                        help='''
+    Trusted SSL Certificate Bundle in PEM (Base64 Encoded DER) Format''')
     parser.add_argument('-y', dest='proxy', required=False,
-                        help='Proxy (host:port format')
+                        help='''
+    Proxy (host:port format''')
 
     args = parser.parse_args()
     credential = {'userName': args.user, 'password': args.passwd}
@@ -82,7 +92,7 @@ def main():
     login(con, credential)
     acceptEULA(con)
 
-    getsans(fcs)
+    get_managed_sans(fcs)
 
 if __name__ == '__main__':
     import sys
