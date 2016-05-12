95a1b1aeea8fdd520d2d125503ef79edf2bef807
Gustavo Augusto Hennig <gustavoh@dbserver.com.br>
2016-05-12 17:44:27 -0300

    Created unit tests for resource and fc_networks

---
+++ hpOneView/test/unit/resources/networking/test_fc_networks.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+###
+# (C) Copyright (2012-2016) Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+###
+
+import mock
+import unittest
+
+from hpOneView.oneview_client import OneViewClient
+from hpOneView.connection import connection
+from hpOneView.resources.networking.fc_networks import FcNetworks
+from hpOneView.resources.resource import ResourceClient
+
+
+class FcNetworksTest(unittest.TestCase):
+    @mock.patch.object(ResourceClient, 'get_all')
+    def test_get_all_called_once(self, mock_get_all):
+        res = FcNetworks(None)
+        filter = 'name=TestName'
+        res.get_all(filter)
+        mock_get_all.assert_called_once_with(filter=filter)
