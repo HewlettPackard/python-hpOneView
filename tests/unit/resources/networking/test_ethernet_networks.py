fe6716fff3008dc1757c1480a80bf04cb8571907
Andrea Dean <andrea.ros.dean@hpe.com>
2016-06-07 09:19:19 -0600

    Added support for ethernet networks

---
+++ tests/unit/resources/networking/test_ethernet_networks.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+###
+# (C) Copyright (2012-2016) Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the 'Software'), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+###
+
+from unittest import TestCase
+
+import mock
+
+from hpOneView.connection import connection
+from hpOneView.resources.networking.ethernet_networks import EthernetNetworks
+from hpOneView.resources.resource import ResourceClient
+
+
+class EthernetNetworksTest(TestCase):
+
+    def setUp(self):
+        self.host = '127.0.0.1'
+        self.connection = connection(self.host)
+        self._ethernet_networks = EthernetNetworks(self.connection)
+
+    @mock.patch.object(ResourceClient, 'get_all')
+    def test_get_all_called_once(self, mock_get_all):
+        filter = 'name=TestName'
+        sort = 'name:ascending'
+
+        self._ethernet_networks.get_all(2, 500, filter, sort)
+
+        mock_get_all.assert_called_once_with(2, 500, filter=filter, sort=sort)
+
+    @mock.patch.object(ResourceClient, 'create')
+    def test_create_should_use_given_values(self, mock_create):
+        resource = {
+            "vlanId": 10,
+            'name': 'OneViewSDK Test Ethernet Network',
+            "ethernetNetworkType": "Tagged",
+            "purpose": "Management",
+            "connectionTemplateUri": None,
+            "smartLink": False,
+            "type": "ethernet-networkV3",
+            "privateNetwork": False
+        }
+        resource_rest_call = resource.copy()
+        mock_create.return_value = {}
+
+        self._ethernet_networks.create(resource, False)
+        mock_create.assert_called_once_with(resource_rest_call, False)
+
+    @mock.patch.object(ResourceClient, 'create')
+    def test_create_should_use_default_values(self, mock_create):
+        resource = {
+            'name': 'OneViewSDK Test Ethernet Network',
+        }
+        resource_with_default_values = {
+            'name': 'OneViewSDK Test Ethernet Network',
+            "ethernetNetworkType": "Tagged",
+            "type": "ethernet-networkV3"
+        }
+        mock_create.return_value = {}
+
+        self._ethernet_networks.create(resource)
+
+        mock_create.assert_called_once_with(resource_with_default_values, True)
+
+    @mock.patch.object(ResourceClient, 'update')
+    def test_update_should_use_given_values(self, mock_update):
+        resource = {
+            'name': 'OneViewSDK Test Ethernet Network',
+            'smartLink': False,
+            'connectionTemplateUri': None,
+            'vlanId': None,
+            'privateNetwork': True,
+            'ethernetNetworkType': 'Untagged',
+            'type': 'ethernet-networkV3',
+            'purpose': 'General'
+        }
+        resource_rest_call = resource.copy()
+        mock_update.return_value = {}
+
+        self._ethernet_networks.update(resource, False)
+        mock_update.assert_called_once_with(resource_rest_call, False)
+
+    @mock.patch.object(ResourceClient, 'update')
+    def test_update_should_use_default_values(self, mock_update):
+        resource = {
+            'name': 'OneViewSDK Test Ethernet Network',
+        }
+        resource_with_default_values = {
+            'type': 'ethernet-networkV3',
+            'name': 'OneViewSDK Test Ethernet Network',
+            'ethernetNetworkType': 'Tagged',
+        }
+        mock_update.return_value = {}
+
+        self._ethernet_networks.update(resource)
+
+        mock_update.assert_called_once_with(resource_with_default_values, True)
+
+    @mock.patch.object(ResourceClient, 'delete')
+    def test_delete_called_once(self, mock_delete):
+        id = 'ad28cf21-8b15-4f92-bdcf-51cb2042db32'
+        self._ethernet_networks.delete(id, force=False, blocking=True)
+
+        mock_delete.assert_called_once_with(id, force=False, blocking=True)
+
+    @mock.patch.object(ResourceClient, 'get_by')
+    def test_get_by_called_once(self, mock_get_by):
+        self._ethernet_networks.get_by(
+            'name', 'OneViewSDK Test Ethernet Network')
+
+        mock_get_by.assert_called_once_with(
+            'name', 'OneViewSDK Test Ethernet Network')
+
+    @mock.patch.object(ResourceClient, 'get')
+    def test_get_called_once(self, mock_get):
+        self._ethernet_networks.get('3518be0e-17c1-4189-8f81-83f3724f6155')
+
+        mock_get.assert_called_once_with(
+            '3518be0e-17c1-4189-8f81-83f3724f6155')
+
+    @mock.patch.object(ResourceClient, 'get')
+    def test_get_with_uri_called_once(self, mock_get):
+        uri = '/rest/ethernet-networks/3518be0e-17c1-4189-8f81-83f3724f6155'
+        self._ethernet_networks.get(uri)
+
+        mock_get.assert_called_once_with(uri)
