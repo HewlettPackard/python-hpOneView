2ee9f96019a71b1df843de7b4ea94eddc662e836
Andrea Dean <andrea.ros.dean@hpe.com>
2016-06-24 09:27:16 -0600

    Extends support for Switches

--- examples/switches.py
+++ examples/switches.py
@@ -42,7 +42,8 @@ oneview_client = OneViewClient(config)
 # Get Statistics
 print("Get a switch statistics")
 try:
-    switch_statistics = oneview_client.switches.get_statistics("30c04831-169e-4618-86b2-7a46310ebaea")
+    switch_statistics = oneview_client.switches.get_statistics(
+        "30c04831-169e-4618-86b2-7a46310ebaea")
     pprint(switch_statistics)
 except HPOneViewException as e:
     print(e.msg['message'])
@@ -50,7 +51,60 @@ except HPOneViewException as e:
 # Get Statistics with port_name
 print("Get a switch statistics with portName")
 try:
-    switch_statistics = oneview_client.switches.get_statistics("30c04831-169e-4618-86b2-7a46310ebaea", "1.2")
+    switch_statistics = oneview_client.switches.get_statistics(
+        "30c04831-169e-4618-86b2-7a46310ebaea", "1.2")
+    switch_statistics = oneview_client.switches.get_statistics(
+        "30c04831-169e-4618-86b2-7a46310ebaea", "1.2")
     pprint(switch_statistics)
+except HPOneViewException as e:
+    print(e.msg['message'])
+
+# Get all switches
+print("Get all switches in domain")
+switches_all = oneview_client.switches.get_all()
+pprint(switches_all)
+
+# Get switch by id
+try:
+    print("Get switch by id")
+    switch_by_id = oneview_client.switches.get(
+        "30c04831-169e-4618-86b2-7a46310ebaea")
+    pprint(switch_by_id)
+except HPOneViewException as e:
+    print(e.msg['message'])
+
+# Get a switch by uri
+try:
+    print("Get switch by uri")
+    switch_by_uri = oneview_client.switches.get(
+        "/rest/switches/30c04831-169e-4618-86b2-7a46310ebaea")
+    pprint(switch_by_uri)
+except HPOneViewException as e:
+    print(e.msg['message'])
+
+# Get environmental configuration of switch by id
+try:
+    print("Get environmental configuration of switch by id")
+    switch_by_id = oneview_client.switches.get_environmental_configuration(
+        "30c04831-169e-4618-86b2-7a46310ebaea")
+    pprint(switch_by_id)
+except HPOneViewException as e:
+    print(e.msg['message'])
+
+# Get environmental configuration of switch by uri
+try:
+    print("Get environmental configuration of switch by uri")
+    switch_by_uri = oneview_client.switches.get_environmental_configuration(
+        "/rest/switches/30c04831-169e-4618-86b2-7a46310ebaea")
+    pprint(switch_by_uri)
+except HPOneViewException as e:
+    print(e.msg['message'])
+
+# Get switch by rackName
+try:
+    print("Get switch by rack name")
+    switch_by_rack_name = oneview_client.switches.get_by(
+        "rackName", "Test Name")
+    pprint(switch_by_rack_name)
 except HPOneViewException as e:
     print(e.msg['message'])
--- tests/unit/resources/networking/test_switches.py
+++ tests/unit/resources/networking/test_switches.py
@@ -31,6 +31,7 @@ from hpOneView.resources.resource import ResourceClient
 
 
 class SwitchesTest(unittest.TestCase):
+
     def setUp(self):
         self.host = '127.0.0.1'
         self.connection = connection(self.host)
@@ -39,15 +40,69 @@ class SwitchesTest(unittest.TestCase):
     @mock.patch.object(ResourceClient, 'get')
     def test_get_statistics_called_once(self, mock_get):
         self._switches.get_statistics('3518be0e-17c1-4189-8f81-83f3724f6155')
-
         uri = '/rest/switches/3518be0e-17c1-4189-8f81-83f3724f6155/statistics'
-
         mock_get.assert_called_once_with(uri)
 
     @mock.patch.object(ResourceClient, 'get')
     def test_get_statistics_with_portName(self, mock_get):
-        self._switches.get_statistics('3518be0e-17c1-4189-8f81-83f3724f6155', 'X1')
-
+        self._switches.get_statistics(
+            '3518be0e-17c1-4189-8f81-83f3724f6155', 'X1')
         uri = '/rest/switches/3518be0e-17c1-4189-8f81-83f3724f6155/statistics/X1'
-
         mock_get.assert_called_once_with(uri)
+
+    @mock.patch.object(ResourceClient, 'get')
+    def test_get_by_id_called_once(self, mock_get):
+        switch_id = "f0a0a113-ec97-41b4-83ce-d7c92b900e7c"
+        self._switches.get(switch_id)
+        mock_get.assert_called_once_with(switch_id)
+
+    @mock.patch.object(ResourceClient, 'get')
+    def test_get_by_uri_called_once(self, mock_get):
+        switch_uri = "/rest/switches/f0a0a113-ec97-41b4-83ce-d7c92b900e7c"
+        self._switches.get(switch_uri)
+        mock_get.assert_called_once_with(switch_uri)
+
+    @mock.patch.object(ResourceClient, 'get_all')
+    def test_get_all_called_once(self, mock_get_all):
+        filter = 'name=TestName'
+        sort = 'name:ascending'
+        self._switches.get_all(2, 500, filter, sort)
+        mock_get_all.assert_called_once_with(2, 500, filter=filter, sort=sort)
+
+    @mock.patch.object(ResourceClient, 'get_all')
+    def test_get_all_called_once_with_defaults(self, mock_get_all):
+        self._switches.get_all()
+        mock_get_all.assert_called_once_with(0, -1, filter='', sort='')
+
+    @mock.patch.object(ResourceClient, 'get')
+    def test_get_environmental_configuration_called_once_when_id_provided(self, mock_get):
+        switches_id = "f0a0a113-ec97-41b4-83ce-d7c92b900e7c"
+        switches_environmental_config_uri = \
+            "/rest/switches/f0a0a113-ec97-41b4-83ce-d7c92b900e7c/environmentalConfiguration"
+        self._switches.get_environmental_configuration(switches_id)
+        mock_get.assert_called_once_with(switches_environmental_config_uri)
+
+    @mock.patch.object(ResourceClient, 'get')
+    def test_get_environmental_configuration_called_once_when_uri_provided(self, mock_get):
+        switches_uri = "/rest/switches/f0a0a113-ec97-41b4-83ce-d7c92b900e7c"
+        switches_environmental_config_uri = \
+            "/rest/switches/f0a0a113-ec97-41b4-83ce-d7c92b900e7c/environmentalConfiguration"
+        self._switches.get_environmental_configuration(switches_uri)
+        mock_get.assert_called_once_with(switches_environmental_config_uri)
+
+    @mock.patch.object(ResourceClient, 'delete')
+    def test_delete_called_once(self, mock_delete):
+        id = 'ad28cf21-8b15-4f92-bdcf-51cb2042db32'
+        self._switches.delete(id, force=True, timeout=50)
+        mock_delete.assert_called_once_with(id, force=True, timeout=50)
+
+    @mock.patch.object(ResourceClient, 'delete')
+    def test_delete_called_once_with_defaults(self, mock_delete):
+        id = 'ad28cf21-8b15-4f92-bdcf-51cb2042db32'
+        self._switches.delete(id)
+        mock_delete.assert_called_once_with(id, force=False, timeout=-1)
+
+    @mock.patch.object(ResourceClient, 'get_by')
+    def test_get_by_called_once(self, mock_get_by):
+        self._switches.get_by("name", "test name")
+        mock_get_by.assert_called_once_with("name", "test name")
