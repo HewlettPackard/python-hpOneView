4e58ea130e93802b549ad976e75373f90971aed7
Camila Balestrin <camila.balestrin@hpe.com>
2016-08-08 18:55:59 -0300

    Add support to get available resources for a server profile

--- examples/server_profiles.py
+++ examples/server_profiles.py
@@ -24,6 +24,7 @@
 from pprint import pprint
 
 from config_loader import try_load_from_file
+from hpOneView.exceptions import HPOneViewException
 from hpOneView.oneview_client import OneViewClient
 
 config = {
@@ -34,28 +35,43 @@ config = {
     }
 }
 
-# To run this sample you must define a server hardware type uri, an enclosure group uri and a server profile id with
-# an associated server profile template
-server_hardware_type_uri = None
-enclosure_group_uri = None
-server_profile_id = None
-
-server_profile_name = "Profile101"
-
 # Try load config from a file (if there is a config file)
 config = try_load_from_file(config)
 
 oneview_client = OneViewClient(config)
 
-# Create a server profile
-print("\nCreate a basic connection-less assigned server profile")
-basic_profile_options = dict(
-    name=server_profile_name,
-    serverHardwareTypeUri=server_hardware_type_uri,
-    enclosureGroupUri=enclosure_group_uri
-)
-basic_profile = oneview_client.server_profiles.create(basic_profile_options)
-pprint(basic_profile)
+# To run this sample you must define a server hardware type uri and an enclosure group uri
+enclosure_group_uri = None
+server_hardware_type_uri = None
+
+# To run the example 'get a specific storage system' you must define a storage system ID
+storage_system_id = None
+
+# To run the example 'get port model associated with a server hardware' you must define a server hardware uri
+server_hardware_uri = None
+
+try:
+    # Create a server profile template to associate with the server profile
+    basic_server_template = oneview_client.server_profile_templates.create(dict(
+        name="ProfileTemplate101",
+        serverHardwareTypeUri=server_hardware_type_uri,
+        enclosureGroupUri=enclosure_group_uri
+    ))
+    server_template_uri = basic_server_template['uri']
+
+    # Create a server profile
+    print("\nCreate a basic connection-less assigned server profile")
+    basic_profile_options = dict(
+        name="Profile101",
+        serverProfileTemplateUri=server_template_uri,
+        serverHardwareTypeUri=server_hardware_type_uri,
+        enclosureGroupUri=enclosure_group_uri
+    )
+    basic_profile = oneview_client.server_profiles.create(basic_profile_options)
+    profile_uri = basic_profile["uri"]
+    pprint(basic_profile)
+except HPOneViewException as e:
+    print(e.msg)
 
 # Update bootMode from recently created profile
 print("\nUpdate bootMode from recently created profile")
@@ -66,7 +82,7 @@ pprint(profile_updated)
 
 # Patch
 print("\nUpdate the profile configuration from server profile template")
-profile_updated = oneview_client.server_profiles.patch(id_or_uri=server_profile_id, operation="replace",
+profile_updated = oneview_client.server_profiles.patch(id_or_uri=profile_uri, operation="replace",
                                                        path="/templateCompliance", value="Compliant")
 pprint(profile_updated)
 
@@ -85,12 +101,11 @@ for profile in profiles:
 
 # Get by name
 print("\nGet a server profile by name")
-profile = oneview_client.server_profiles.get_by_name(server_profile_name)
+profile = oneview_client.server_profiles.get_by_name("Profile101")
 pprint(profile)
 
 # Get by uri
 print("\nGet a server profile by uri")
-profile_uri = all_profiles[0]["uri"]
 profile = oneview_client.server_profiles.get(profile_uri)
 pprint(profile)
 
@@ -99,11 +114,14 @@ print("\nRetrieve the generated ServerProfile schema")
 schema = oneview_client.server_profiles.get_schema()
 pprint(schema)
 
-# Server profile compliance preview
-print("\nGets the preview of manual and automatic updates required to make the server profile consistent "
-      "with its template.")
-schema = oneview_client.server_profiles.get_compliance_preview(server_profile_id)
-pprint(schema)
+try:
+    # Server profile compliance preview
+    print("\nGets the preview of manual and automatic updates required to make the server profile consistent "
+          "with its template.")
+    schema = oneview_client.server_profiles.get_compliance_preview(profile_uri)
+    pprint(schema)
+except HPOneViewException as e:
+    print(e.msg)
 
 # Get profile ports
 print("\nRetrieve the port model associated with a server hardware type and enclosure group")
@@ -111,18 +129,22 @@ profile_ports = oneview_client.server_profiles.get_profile_ports(enclosureGroupU
                                                                  serverHardwareTypeUri=server_hardware_type_uri)
 pprint(profile_ports)
 
-# Get profile ports
-# To run this example you must define a server hardware uri
-server_hardware_uri = None
-if server_hardware_uri:
-    print("\nRetrieve the port model associated with a server hardware")
-    profile_ports = oneview_client.server_profiles.get_profile_ports(serverHardwareUri=server_hardware_uri)
-    pprint(profile_ports)
+try:
+    # Get profile ports
+    if server_hardware_uri:
+        print("\nRetrieve the port model associated with a server hardware")
+        profile_ports = oneview_client.server_profiles.get_profile_ports(serverHardwareUri=server_hardware_uri)
+        pprint(profile_ports)
+except HPOneViewException as e:
+    print(e.msg)
 
-# Retrieve the error or status messages associated with the specified profile
-print("\nList profile status messages associated with a profile")
-messages = oneview_client.server_profiles.get_messages(server_profile_id)
-pprint(messages)
+try:
+    # Retrieve the error or status messages associated with the specified profile
+    print("\nList profile status messages associated with a profile")
+    messages = oneview_client.server_profiles.get_messages(profile_uri)
+    pprint(messages)
+except HPOneViewException as e:
+    print(e.msg)
 
 try:
     # Transform an server profile
@@ -133,10 +155,60 @@ try:
     print("Transformation complete. Updating server profile with the new configuration.")
     profile_updated = oneview_client.server_profiles.update(server_transformed, server_transformed["uri"])
     pprint(profile_updated)
-except Exception as e:
-    print(e.args[0])
+except HPOneViewException as e:
+    print(e.msg)
+
+try:
+    # Get the list of networks and network sets that are available to a server profile along with their respective ports
+    print("\nList all Ethernet networks associated with a server hardware type and enclosure group")
+    available_networks = oneview_client.server_profiles.get_available_networks(
+        enclosureGroupUri=enclosure_group_uri, serverHardwareTypeUri=server_hardware_type_uri, view='Ethernet')
+    pprint(available_networks)
+except HPOneViewException as e:
+    print(e.msg)
+
+try:
+    # Get the list of available servers
+    print("\nList all available servers associated with a server hardware type and enclosure group")
+    available_servers = oneview_client.server_profiles.get_available_servers(
+        enclosureGroupUri=enclosure_group_uri, serverHardwareTypeUri=server_hardware_type_uri)
+    pprint(available_servers)
+except HPOneViewException as e:
+    print(e.msg)
+
+try:
+    # List available storage systems
+    print("\nList available storage systems associated with the given enclosure group URI and server hardware type URI")
+    available_storage_systems = oneview_client.server_profiles.get_available_storage_systems(
+        count=25, start=0, enclosureGroupUri=enclosure_group_uri, serverHardwareTypeUri=server_hardware_type_uri)
+    pprint(available_storage_systems)
+except HPOneViewException as e:
+    print(e.msg)
+
+try:
+    # Get a specific storage system
+    if storage_system_id:
+        print("\nRetrieve a specific storage system associated with the given enclosure group URI, a server hardware"
+              " type URI and a storage system ID")
+        available_storage_system = oneview_client.server_profiles.get_available_storage_system(
+            storageSystemId=storage_system_id, enclosureGroupUri=enclosure_group_uri,
+            serverHardwareTypeUri=server_hardware_type_uri)
+        pprint(available_storage_system)
+except HPOneViewException as e:
+    print(e.msg)
+
+try:
+    # List available targets
+    print("\nList all available servers and bays for a given enclosure group.")
+    available_targets = oneview_client.server_profiles.get_available_targets(enclosureGroupUri=enclosure_group_uri)
+    pprint(available_targets)
+except HPOneViewException as e:
+    print(e.msg)
 
 # Delete the created server profile
 print("\nDelete the created server profile")
 oneview_client.server_profiles.delete(basic_profile)
 print("The server profile was successfully deleted.")
+
+# Delete the created server profile template
+oneview_client.server_profile_templates.delete(basic_server_template)
