0dbc491f502c856d83142b2e6e1679feac3b21d7
Thiago Miotto <tmiotto@hpe.com>
2017-07-19 16:42:45 +0000

    Storage Pools for API500

--- examples/storage_pools_store_serv.py
+++ examples/storage_pools_store_serv.py
@@ -49,35 +49,39 @@ if s_systems:
         s_system['name'], s_system['uri']))
 else:
     options = {
-        "ip_hostname": config['storage_system_hostname'],
+        "hostname": config['storage_system_hostname'],
         "username": config['storage_system_username'],
-        "password": config['storage_system_password']
+        "password": config['storage_system_password'],
+        "family": config['storage_system_family']
     }
     s_system = oneview_client.storage_systems.add(options)
-    s_system['managedDomain'] = s_system['unmanagedDomains'][0]
+    s_system['deviceSpecificAttributes']['managedDomain'] = s_system[
+        'deviceSpecificAttributes']['discoveredDomains'][0]
+    s_system['deviceSpecificAttributes']['managedPools'] = s_system[
+        'deviceSpecificAttributes']['discoveredPools']
     s_system = oneview_client.storage_systems.update(s_system)
     storage_system_added = True
     print("   Added storage system '{}' at uri: {}".format(
         s_system['name'], s_system['uri']))
 
 # Find and add unmanaged storage pool for management
-pool_name = ''
-storage_pool_add = {}
-print("Find and add unmanaged storage pool for management")
-for pool in s_system['unmanagedPools']:
-    if pool['domain'] == s_system['managedDomain']:
-        pool_name = pool['name']
-        break
-if pool_name:
-    print("   Found pool '{}'".format(pool_name))
-    options = {
-        "storageSystemUri": s_system['uri'],
-        "poolName": pool_name
-    }
-    storage_pool_add = oneview_client.storage_pools.add(options)
-    print("   Successfully added pool")
-else:
-    print("   No available unmanaged storage pools to add")
+# pool_name = ''
+# storage_pool_add = {}
+# print("Find and add unmanaged storage pool for management")
+# for pool in s_system['unmanagedPools']:
+#     if pool['domain'] == s_system['managedDomain']:
+#         pool_name = pool['name']
+#         break
+# if pool_name:
+#     print("   Found pool '{}'".format(pool_name))
+#     options = {
+#         "storageSystemUri": s_system['uri'],
+#         "poolName": pool_name
+#     }
+#     storage_pool_add = oneview_client.storage_pools.add(options)
+#     print("   Successfully added pool")
+# else:
+#     print("   No available unmanaged storage pools to add")
 
 # Get all managed storage pools
 print("Get all managed storage pools")
@@ -86,39 +90,39 @@ for pool in storage_pools_all:
     print("   '{}' at uri: '{}'".format(pool['name'], pool['uri']))
 
 # Get maximum of 5 storage pools sorted by freeCapacity in descending order.
-print(
-    "Get maximum of 5 storage pools  sorted by freeCapacity in descending order.")
-storage_pools_filtered = oneview_client.storage_pools.get_all(
-    0, 5, sort='freeCapacity:desc')
-for pool in storage_pools_filtered:
-    print("   '{}' at uri: '{}'".format(
-        pool['name'], pool['uri']))
-
-# Get storage pool by uri
-storage_pool_by_uri = oneview_client.storage_pools.get(
-    storage_pools_all[0]['uri'])
-print("Got storage pool '{}' by   uri: '{}'".format(
-    storage_pool_by_uri['name'], storage_pool_by_uri['uri']))
-pprint(storage_pool_by_uri)
-
-# Get storage pool by id
-try:
-    storage_pool_by_id = oneview_client.storage_pools.get(
-        'CFA635CC-A9FB-4883-9B4B-BF65FA68FA57')
-    print("Got storage pool '{}' by id 'CFA635CC-A9FB-4883-9B4B-BF65FA68FA57' at\n   uri: '{}'".format(
-        storage_pool_by_id['name'], storage_pool_by_id['uri']))
-    pprint(storage_pool_by_id)
-except HPOneViewException as e:
-    print(e.msg)
-
-# Remove storage pool
-if storage_pool_add:
-    print("Remove recently added storage pool")
-    oneview_client.storage_pools.remove(storage_pool_add)
-    print("   Done.")
-
-# Remove storage system, if it was added
-if storage_system_added:
-    print("Remove recently added storage system")
-    oneview_client.storage_systems.remove(s_system)
-    print("   Done.")
+# print(
+#     "Get maximum of 5 storage pools  sorted by freeCapacity in descending order.")
+# storage_pools_filtered = oneview_client.storage_pools.get_all(
+#     0, 5, sort='freeCapacity:desc')
+# for pool in storage_pools_filtered:
+#     print("   '{}' at uri: '{}'".format(
+#         pool['name'], pool['uri']))
+#
+# # Get storage pool by uri
+# storage_pool_by_uri = oneview_client.storage_pools.get(
+#     storage_pools_all[0]['uri'])
+# print("Got storage pool '{}' by   uri: '{}'".format(
+#     storage_pool_by_uri['name'], storage_pool_by_uri['uri']))
+# pprint(storage_pool_by_uri)
+#
+# # Get storage pool by id
+# try:
+#     storage_pool_by_id = oneview_client.storage_pools.get(
+#         'CFA635CC-A9FB-4883-9B4B-BF65FA68FA57')
+#     print("Got storage pool '{}' by id 'CFA635CC-A9FB-4883-9B4B-BF65FA68FA57' at\n   uri: '{}'".format(
+#         storage_pool_by_id['name'], storage_pool_by_id['uri']))
+#     pprint(storage_pool_by_id)
+# except HPOneViewException as e:
+#     print(e.msg)
+#
+# # Remove storage pool
+# if storage_pool_add:
+#     print("Remove recently added storage pool")
+#     oneview_client.storage_pools.remove(storage_pool_add)
+#     print("   Done.")
+#
+# # Remove storage system, if it was added
+# if storage_system_added:
+#     print("Remove recently added storage system")
+#     oneview_client.storage_systems.remove(s_system)
+#     print("   Done.")
