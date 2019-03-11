0d58b8ae339ef9de8346fc528b66efc7a6a66553
sijeesh <mailtosije@gmail.com>
2019-03-11 15:48:58 +0530

    Updated resource file and example

--- hpOneView/resources/resource.py
+++ hpOneView/resources/resource.py
@@ -179,8 +179,8 @@ class Resource(object):
         if not data:
             data = {}
 
-        default_values = self._get_default_values()
-        data = self._helper.add_new_fields(data, default_values)
+        default_values = self.get_default_values()
+        data = self._helper.update_resource_fields(data, default_values)
 
         logger.debug('Create (uri = %s, resource = %s)' % (uri, str(data)))
 
@@ -296,12 +296,15 @@ class Resource(object):
 
         return new_resource
 
-    def _get_default_values(self):
+    def get_default_values(self, default_values=None):
         """Gets the default values set for a resource"""
 
-        if self.DEFAULT_VALUES:
+        if not default_values:
+            default_values = self.DEFAULT_VALUES
+
+        if default_values:
             api_version = str(self._connection._apiVersion)
-            values = self.DEFAULT_VALUES.get(api_version, {}).copy()
+            values = default_values.get(api_version, {}).copy()
         else:
             values = {}
 
@@ -309,7 +312,7 @@ class Resource(object):
 
     def _merge_default_values(self):
         """Merge default values with resource data."""
-        values = self._get_default_values()
+        values = self.get_default_values()
         for key, value in values.items():
             if not self.data.get(key):
                 self.data[key] = value
@@ -572,6 +575,22 @@ class ResourceHelper(object):
         else:
             return []
 
+    def update_resource_fields(self, data, data_to_add):
+       """Update resource data with new fields.
+
+       Args:
+           data: resource data
+           data_to_update: dict of data to update resource data
+
+       Returnes:
+           Returnes dict
+       """
+       for key, value in data_to_add.items():
+            if not data.get(key):
+                data[key] = value
+
+       return data
+
     def do_requests_to_getall(self, uri, requested_count):
         """Helps to make http request for get_all method.
 
@@ -676,6 +695,7 @@ class ResourceHelper(object):
 
 class ResourcePatchMixin(object):
 
+    @ensure_resource_client
     def patch(self, operation, path, value, custom_headers=None, timeout=-1):
         """Uses the PATCH to update a resource.
 
