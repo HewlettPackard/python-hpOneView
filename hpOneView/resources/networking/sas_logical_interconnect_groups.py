729734e331ab8f5aea2cc0dc81f317ab27c4d4e3
sijeesh <sijeesh.kattumunda@hpe.com>
2019-03-18 16:30:22 +0530

    SAS Logical interconnect groups - updated resource module and examples

--- hpOneView/resources/networking/sas_logical_interconnect_groups.py
+++ hpOneView/resources/networking/sas_logical_interconnect_groups.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ###
-# (C) Copyright (2012-2017) Hewlett Packard Enterprise Development LP
+# (C) Copyright (2012-2019) Hewlett Packard Enterprise Development LP
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -30,10 +30,10 @@ from future import standard_library
 standard_library.install_aliases()
 
 
-from hpOneView.resources.resource import ResourceClient
+from hpOneView.resources.resource import Resource
 
 
-class SasLogicalInterconnectGroups(object):
+class SasLogicalInterconnectGroups(Resource):
     """
     SAS Logical Interconnect Groups API client.
 
@@ -47,10 +47,11 @@ class SasLogicalInterconnectGroups(object):
         '300': {'type': 'sas-logical-interconnect-group'},
         '500': {'type': 'sas-logical-interconnect-group'},
         '600': {'type': 'sas-logical-interconnect-groupV2'},
+        '800': {'type': 'sas-logical-interconnect-groupV2'},
     }
 
-    def __init__(self, con):
-        self._client = ResourceClient(con, self.URI)
+    def __init__(self, connection, data=None):
+        super(SasLogicalInterconnectGroups, self).__init__(connection, data)
 
     def get_all(self, start=0, count=-1, filter='', sort='', scope_uris=''):
         """
@@ -79,84 +80,5 @@ class SasLogicalInterconnectGroups(object):
         Returns:
             list: A list of SAS logical interconnect groups.
         """
-        return self._client.get_all(start, count, filter=filter, sort=sort, scope_uris=scope_uris)
+        return self._helper.get_all(start, count, filter=filter, sort=sort, scope_uris=scope_uris)
 
-    def get(self, id_or_uri):
-        """
-        Gets the SAS logical interconnect group.
-
-        Args:
-            id_or_uri: ID or URI of a SAS logical interconnect group.
-
-        Returns:
-            dict: SAS logical interconnect group.
-        """
-        return self._client.get(id_or_uri)
-
-    def get_by(self, field, value):
-        """
-        Gets all SAS logical interconnect groups that match the filter.
-
-        The search is case-insensitive.
-
-        Args:
-            field: Field name to filter.
-            value: Value to filter.
-
-        Returns:
-            list: A list of SAS logical interconnect groups.
-        """
-        return self._client.get_by(field, value)
-
-    def create(self, resource, timeout=-1):
-        """
-        Creates a SAS logical interconnect group.
-
-        Args:
-            resource (dict): Object to create.
-            timeout:
-                Timeout in seconds. Wait for task completion by default. The timeout does not abort the operation
-                in OneView, just stop waiting for its completion.
-
-        Returns:
-            dict: Created resource.
-
-        """
-        return self._client.create(resource, timeout=timeout, default_values=self.DEFAULT_VALUES)
-
-    def update(self, resource, timeout=-1):
-        """
-        Updates a SAS logical interconnect group.
-
-        Args:
-            resource (dict): Object to update.
-            timeout:
-                Timeout in seconds. Wait for task completion by default. The timeout does not abort the operation
-                in OneView, just stop waiting for its completion.
-
-        Returns:
-            dict: Updated resource.
-
-        """
-        return self._client.update(resource, timeout=timeout, default_values=self.DEFAULT_VALUES)
-
-    def delete(self, resource, force=False, timeout=-1):
-        """
-        Removes a SAS logical interconnect group. You cannot delete a SAS logical interconnect group when a
-        corresponding SAS logical interconnect is still referencing it. Delete the logical enclosure before
-        attempting to delete the SAS logical interconnect it uses.
-
-        Args:
-            resource: object to delete
-            force:
-                 If set to true, the operation completes despite any problems with
-                 network connectivity or errors on the resource itself. The default is false.
-            timeout:
-                Timeout in seconds. Wait for task completion by default. The timeout does not abort the operation
-                in OneView; it just stops waiting for its completion.
-
-        Returns:
-            bool: Indicates if the resource was deleted successfully.
-
-        """
-        return self._client.delete(resource, force=force, timeout=timeout)
